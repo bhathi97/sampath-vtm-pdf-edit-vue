f244b65e59bb79116765d5b540446bc75f1537be
bhathi97 <bhathiya.rb@gmail.com>
2024-03-14 09:34:38 +0530

    first commit

---
+++ sampath-vtm-pdf/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Home/>
+  </div>
+</template>
+
+<script>
+
+import Home from '@/components/Home'
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+
+  overflow: auto;
+  position: relative;
+}
+</style>
